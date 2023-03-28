93e7c1ef2b45295286eeca46521c560c25700ec2
sigmdel <michel@sigmdel.ca>
2023-03-27 22:18:32 -0300

    Commit project 05_websocket_update

---
+++ 05_websocket_update/websocket_update/html.h
@@ -0,0 +1,93 @@
+#include <Arduino.h>
+
+const char html_index[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>%TITLE%</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <link rel="icon" href="data:,">
+  <style>
+    html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center; background-color: white;}
+    h1{color: #0F3376; padding: 2vh;}
+    table{margin-left: auto; margin-right: auto; margin-top:20px;}
+    td{font-size: 1.5rem; text-align: left; padding: 8px;}
+    .state{font-size: 2rem; font-weight: bold;margin-top:28px;}
+    .button{display: inline-block; background-color: blue; border: none; border-radius: 6px; color: white; font-size: 1.5rem; width: 5em; height: 3em; text-decoration: none; margin: 2px; cursor: pointer;}
+    .info{margin-top:48px;}
+  </style>
+</head>
+<body>
+  <h1>%DEVICENAME%</h1>
+  <table>
+  <tr><td>Temperature:</td><td><b><span id="temp">%TEMPERATURE%</span></b> °C</td></tr>
+  <tr><td>Humidity:</td><td><b><span id="humd">%HUMIDITY%</span></b> &percnt;</td></tr>
+  <tr><td>Brightness:</td><td><b><span id="light">%LIGHT%</span></b></td></tr>
+  </table>
+  <div class="state"><span id="led">%LEDSTATUS%</span></div>
+  <p><button id="button" class="button">Toggle</button></p>
+  <div class="info">%INFO%</div>
+  <script>
+    var gateway = `ws://${window.location.hostname}/ws`;
+    var websocket;
+    window.addEventListener('load', onLoad);
+    function initWebSocket() {
+      console.log('Trying to open a WebSocket connection...');
+      websocket = new WebSocket(gateway);
+      websocket.onopen    = onOpen;
+      websocket.onclose   = onClose;
+      websocket.onmessage = onMessage; // <-- add this line
+    }
+    function onOpen(event) {
+      console.log('Connection opened');
+    }
+    function onClose(event) {
+      console.log('Connection closed');
+      setTimeout(initWebSocket, 2000);
+    }
+    function onMessage(event) {
+      console.log('onMessage', event.data)
+      const resp = event.data.split(/\r?\n/);
+      console.log("OnMessage data", resp);
+      document.getElementById("led").innerHTML = resp[0];
+      document.getElementById("temp").innerHTML = resp[1];
+      document.getElementById("humd").innerHTML = resp[2];
+      document.getElementById("light").innerHTML = resp[3];
+    }
+    function onLoad(event) {
+      initWebSocket();
+      initButton();
+    }
+    function initButton() {
+      document.getElementById('button').addEventListener('click', toggle);
+    }
+    function toggle(){
+      websocket.send('toggle');
+    }
+  </script>
+</body>
+</html>)rawliteral";
+
+
+const char html_404[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>%DEVICENAME%</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <link rel="icon" href="data:,">
+  <style>
+    html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center; background-color: white;}
+    h1{color: #0F3376; padding: 2vh;}
+    p{font-size: 1.5rem;}
+    .button{display: inline-block; background-color: green; border: none; border-radius: 6px; color: white; font-size: 1.5rem; width: 5em; height: 3em; text-decoration: none; margin: 2px; cursor: pointer;}
+  </style>
+</head>
+<body>
+  <h1>%DEVICENAME%</h1>
+  <p><b>404 Error</b></p>
+  <p><form action='/' method='get'><button class="button">Return</button></form></p>
+</body>
+</html>)rawliteral";
